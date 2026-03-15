7cbfccb7267d2b1abdccff24934680484aebe2f9
Aaron Pribadi <aaron.pribadi@gmail.com>
2026-03-15 16:36:57 -0700

    z

--- benches/unified/bench_map.rs
+++ benches/unified/bench_map.rs
@@ -1,14 +1,21 @@
 use std::num::NonZeroU64;
 use std::array;
 use divan::Bencher;
+use divan::black_box;
 use crate::util::Map;
 
-const N: usize = 10;
-const C: usize = 500;
-const K: usize = 100;
-
-const _: () = assert!(N * C == 5_000); // total working set
-const _: () = assert!(K * N * C * 2 == 1_000_000); // number of operations
+const SIZES: [usize; 10] = [
+  500,
+  536,
+  574,
+  616,
+  660,
+  707,
+  758,
+  812,
+  871,
+  933,
+];
 
 fn make_key(x: usize) -> NonZeroU64 {
   return unsafe { NonZeroU64::new_unchecked((x as u64).rotate_left(16) | 1) };
@@ -17,45 +24,52 @@ fn make_key(x: usize) -> NonZeroU64 {
 #[divan::bench(
   sample_count = 9,
   types = [
-    tangerine::map::HashMap<NonZeroU64, u64>,
-    ahash::AHashMap<NonZeroU64, u64>,
-    foldhash::HashMap<NonZeroU64, u64>,
+    ahash::AHashMap<NonZeroU64, NonZeroU64>,
+    foldhash::HashMap<NonZeroU64, NonZeroU64>,
+    tangerine::map::HashMap<NonZeroU64, NonZeroU64>,
   ])]
 #[inline(never)]
-fn bench_get<T: Map>(bencher: Bencher<'_, '_>) {
-  // TODO:
-  // - different sized tables
-  // - make next lookup depend on result of previous
-  let mut t: [_; N] = array::from_fn(|_| T::new());
-  for i in 0 .. N {
-    let t = &mut t[i];
-    for j in 0 .. C {
-      t.insert(make_key(j), j as u64);
-    }
-  }
+fn bench_get<T: Map<NonZeroU64>>(bencher: Bencher<'_, '_>) {
+  let mut t: [_; 10] =
+    SIZES.map(|n| {
+      let mut t = T::new();
+      for i in 0 .. n { t.insert(make_key(i), make_key(i + 1)); }
+      t
+    });
 
   bencher.bench_local(|| {
-    let mut a = 0;
-    for _ in 0 .. K {
-      for i in 0 .. N {
-        let t = &mut t[i];
-        for j in 0 .. C { if let Some(&y) = t.get(make_key(j)) { a += y; } }
-        for j in 0 .. C { if let Some(&y) = t.get(make_key(j)) { a += y; } }
+    let mut n = 1_000_000;
+    let mut k = make_key(0);
+    'done: loop {
+      for t in t.iter_mut() {
+        for _ in 0 .. 1000 {
+          k = match t.get(k) { None => make_key(0), Some(&y) => y };
+          n = n - 1;
+          if n == 0 { break 'done; }
+        }
       }
     }
-    divan::black_box(a)
+    black_box(k)
   });
 }
+
+const N: usize = 10;
+const C: usize = 500;
+const K: usize = 100;
+
+const _: () = assert!(N * C == 5_000); // total working set
+const _: () = assert!(K * N * C * 2 == 1_000_000); // number of operations
+
 
 #[divan::bench(
   sample_count = 9,
   types = [
-    tangerine::map::HashMap<NonZeroU64, u64>,
     ahash::AHashMap<NonZeroU64, u64>,
     foldhash::HashMap<NonZeroU64, u64>,
+    tangerine::map::HashMap<NonZeroU64, u64>,
   ])]
 #[inline(never)]
-fn bench_insert<T: Map>(bencher: Bencher<'_, '_>) {
+fn bench_insert<T: Map<u64>>(bencher: Bencher<'_, '_>) {
   let mut t: [_; N] = array::from_fn(|_| T::new());
   bencher.bench_local(|| {
     for _ in 0 .. K {
@@ -73,12 +87,12 @@ fn bench_insert<T: Map>(bencher: Bencher<'_, '_>) {
 #[divan::bench(
   sample_count = 9,
   types = [
-    tangerine::map::HashMap<NonZeroU64, u64>,
     ahash::AHashMap<NonZeroU64, u64>,
     foldhash::HashMap<NonZeroU64, u64>,
+    tangerine::map::HashMap<NonZeroU64, u64>,
   ])]
 #[inline(never)]
-fn bench_insert_remove<T: Map>(bencher: Bencher<'_, '_>) {
+fn bench_insert_remove<T: Map<u64>>(bencher: Bencher<'_, '_>) {
   let mut t: [_; N] = array::from_fn(|_| T::new());
   bencher.bench_local(|| {
     for _ in 0 .. K {
--- benches/unified/util.rs
+++ benches/unified/util.rs
@@ -1,52 +1,52 @@
 use std::num::NonZeroU64;
 
-pub(crate) trait Map {
+pub(crate) trait Map<T> {
   fn new() -> Self;
 
-  fn get(&self, _: NonZeroU64) -> Option<&u64>;
+  fn get(&self, _: NonZeroU64) -> Option<&T>;
 
-  fn insert(&mut self, _: NonZeroU64, _: u64);
+  fn insert(&mut self, _: NonZeroU64, _: T);
 
   fn remove(&mut self, _: NonZeroU64);
 }
 
-impl Map for tangerine::map::HashMap<NonZeroU64, u64> {
+impl<T> Map<T> for tangerine::map::HashMap<NonZeroU64, T> {
   #[inline(always)]
   fn new() -> Self { tangerine::map::HashMap::new() }
 
   #[inline(always)]
-  fn get(&self, k: NonZeroU64) -> Option<&u64> { self.get(k) }
+  fn get(&self, k: NonZeroU64) -> Option<&T> { self.get(k) }
 
   #[inline(always)]
-  fn insert(&mut self, k: NonZeroU64, v: u64) { self.insert(k, v); }
+  fn insert(&mut self, k: NonZeroU64, v: T) { self.insert(k, v); }
 
   #[inline(always)]
   fn remove(&mut self, k: NonZeroU64) { self.remove(k); }
 }
 
-impl Map for ahash::AHashMap<NonZeroU64, u64> {
+impl<T> Map<T> for ahash::AHashMap<NonZeroU64, T> {
   #[inline(always)]
   fn new() -> Self { ahash::AHashMap::new() }
 
   #[inline(always)]
-  fn get(&self, k: NonZeroU64) -> Option<&u64> { self.get(&k) }
+  fn get(&self, k: NonZeroU64) -> Option<&T> { self.get(&k) }
 
   #[inline(always)]
-  fn insert(&mut self, k: NonZeroU64, v: u64) { let _: Option<_> = self.insert(k, v); }
+  fn insert(&mut self, k: NonZeroU64, v: T) { let _: Option<_> = self.insert(k, v); }
 
   #[inline(always)]
   fn remove(&mut self, k: NonZeroU64) { let _: Option<_> = self.remove(&k); }
 }
 
-impl Map for foldhash::HashMap<NonZeroU64, u64> {
+impl<T> Map<T> for foldhash::HashMap<NonZeroU64, T> {
   #[inline(always)]
   fn new() -> Self { <foldhash::HashMap<_, _> as foldhash::HashMapExt>::new() }
 
   #[inline(always)]
-  fn get(&self, k: NonZeroU64) -> Option<&u64> { self.get(&k) }
+  fn get(&self, k: NonZeroU64) -> Option<&T> { self.get(&k) }
 
   #[inline(always)]
-  fn insert(&mut self, k: NonZeroU64, v: u64) { let _: Option<_> = self.insert(k, v); }
+  fn insert(&mut self, k: NonZeroU64, v: T) { let _: Option<_> = self.insert(k, v); }
 
   #[inline(always)]
   fn remove(&mut self, k: NonZeroU64) { let _: Option<_> = self.remove(&k); }
