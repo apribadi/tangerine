a24a675a93b43da414e434be127290ec21e54a30
Aaron Pribadi <aaron.pribadi@gmail.com>
2026-03-14 22:44:46 -0700

    z

--- benches/unified/bench_map.rs
+++ benches/unified/bench_map.rs
@@ -22,6 +22,39 @@ fn make_key(x: usize) -> NonZeroU64 {
     foldhash::HashMap<NonZeroU64, u64>,
   ])]
 #[inline(never)]
+fn bench_get<T: Map>(bencher: Bencher<'_, '_>) {
+  // TODO:
+  // - different sized tables
+  // - make next lookup depend on result of previous
+  let mut t: [_; N] = array::from_fn(|_| T::new());
+  for i in 0 .. N {
+    let t = &mut t[i];
+    for j in 0 .. C {
+      t.insert(make_key(j), j as u64);
+    }
+  }
+
+  bencher.bench_local(|| {
+    let mut a = 0;
+    for _ in 0 .. K {
+      for i in 0 .. N {
+        let t = &mut t[i];
+        for j in 0 .. C { if let Some(&y) = t.get(make_key(j)) { a += y; } }
+        for j in 0 .. C { if let Some(&y) = t.get(make_key(j)) { a += y; } }
+      }
+    }
+    divan::black_box(a)
+  });
+}
+
+#[divan::bench(
+  sample_count = 9,
+  types = [
+    tangerine::map::HashMap<NonZeroU64, u64>,
+    ahash::AHashMap<NonZeroU64, u64>,
+    foldhash::HashMap<NonZeroU64, u64>,
+  ])]
+#[inline(never)]
 fn bench_insert<T: Map>(bencher: Bencher<'_, '_>) {
   let mut t: [_; N] = array::from_fn(|_| T::new());
   bencher.bench_local(|| {
