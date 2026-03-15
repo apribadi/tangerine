814ec0180fa8d61316f0831b79dd26eeffe14ca4
Aaron Pribadi <aaron.pribadi@gmail.com>
2026-03-14 20:55:36 -0700

    z

--- benches/unified/bench_map.rs
+++ benches/unified/bench_map.rs
@@ -1,5 +1,7 @@
 use std::num::NonZeroU64;
 use std::array;
+use divan::Bencher;
+use crate::util::Map;
 
 const N: usize = 10;
 const C: usize = 500;
@@ -12,58 +14,46 @@ fn make_key(x: usize) -> NonZeroU64 {
   return unsafe { NonZeroU64::new_unchecked((x as u64).rotate_left(16) | 1) };
 }
 
+#[divan::bench(
+  sample_count = 9,
+  types = [
+    tangerine::map::HashMap<NonZeroU64, u64>,
+    ahash::AHashMap<NonZeroU64, u64>,
+    foldhash::HashMap<NonZeroU64, u64>,
+  ])]
 #[inline(never)]
-fn insert_only<T: crate::util::Map>() {
+fn bench_insert<T: Map>(bencher: Bencher<'_, '_>) {
   let mut t: [_; N] = array::from_fn(|_| T::new());
-  for _ in 0 .. K {
-    for i in 0 .. N {
-      let t = &mut t[i];
-      *t = T::new();
-      for x in 0 .. C { t.insert(make_key(x), x as u64); }
-      *t = T::new();
-      for x in 0 .. C { t.insert(make_key(x), x as u64); }
+  bencher.bench_local(|| {
+    for _ in 0 .. K {
+      for i in 0 .. N {
+        let t = &mut t[i];
+        *t = T::new();
+        for x in 0 .. C { t.insert(make_key(x), x as u64); }
+        *t = T::new();
+        for x in 0 .. C { t.insert(make_key(x), x as u64); }
+      }
     }
-  }
+  });
 }
 
+#[divan::bench(
+  sample_count = 9,
+  types = [
+    tangerine::map::HashMap<NonZeroU64, u64>,
+    ahash::AHashMap<NonZeroU64, u64>,
+    foldhash::HashMap<NonZeroU64, u64>,
+  ])]
 #[inline(never)]
-fn insert_remove<T: crate::util::Map>() {
+fn bench_insert_remove<T: Map>(bencher: Bencher<'_, '_>) {
   let mut t: [_; N] = array::from_fn(|_| T::new());
-  for _ in 0 .. K {
-    for i in 0 .. N {
-      let t = &mut t[i];
-      for x in 0 .. C { t.insert(make_key(x), x as u64); }
-      for x in 0 .. C { t.remove(make_key(x)); }
+  bencher.bench_local(|| {
+    for _ in 0 .. K {
+      for i in 0 .. N {
+        let t = &mut t[i];
+        for x in 0 .. C { t.insert(make_key(x), x as u64); }
+        for x in 0 .. C { t.remove(make_key(x)); }
+      }
     }
-  }
-}
-
-#[divan::bench]
-fn insert_only_tangerine() {
-  insert_only::<tangerine::map::HashMap<NonZeroU64, u64>>();
-}
-
-#[divan::bench]
-fn insert_only_ahash() {
-  insert_only::<ahash::AHashMap<NonZeroU64, u64>>();
-}
-
-#[divan::bench]
-fn insert_only_foldhash() {
-  insert_only::<foldhash::HashMap<NonZeroU64, u64>>();
-}
-
-#[divan::bench]
-fn insert_remove_tangerine() {
-  insert_remove::<tangerine::map::HashMap<NonZeroU64, u64>>();
-}
-
-#[divan::bench]
-fn insert_remove_ahash() {
-  insert_remove::<ahash::AHashMap<NonZeroU64, u64>>();
-}
-
-#[divan::bench]
-fn insert_remove_foldhash() {
-  insert_remove::<foldhash::HashMap<NonZeroU64, u64>>();
+  });
 }
--- benches/unified/util.rs
+++ benches/unified/util.rs
@@ -3,6 +3,8 @@ use std::num::NonZeroU64;
 pub(crate) trait Map {
   fn new() -> Self;
 
+  fn get(&self, _: NonZeroU64) -> Option<&u64>;
+
   fn insert(&mut self, _: NonZeroU64, _: u64);
 
   fn remove(&mut self, _: NonZeroU64);
@@ -11,6 +13,9 @@ pub(crate) trait Map {
 impl Map for tangerine::map::HashMap<NonZeroU64, u64> {
   #[inline(always)]
   fn new() -> Self { tangerine::map::HashMap::new() }
+
+  #[inline(always)]
+  fn get(&self, k: NonZeroU64) -> Option<&u64> { self.get(k) }
 
   #[inline(always)]
   fn insert(&mut self, k: NonZeroU64, v: u64) { self.insert(k, v); }
@@ -24,6 +29,9 @@ impl Map for ahash::AHashMap<NonZeroU64, u64> {
   fn new() -> Self { ahash::AHashMap::new() }
 
   #[inline(always)]
+  fn get(&self, k: NonZeroU64) -> Option<&u64> { self.get(&k) }
+
+  #[inline(always)]
   fn insert(&mut self, k: NonZeroU64, v: u64) { let _: Option<_> = self.insert(k, v); }
 
   #[inline(always)]
@@ -33,6 +41,9 @@ impl Map for ahash::AHashMap<NonZeroU64, u64> {
 impl Map for foldhash::HashMap<NonZeroU64, u64> {
   #[inline(always)]
   fn new() -> Self { <foldhash::HashMap<_, _> as foldhash::HashMapExt>::new() }
+
+  #[inline(always)]
+  fn get(&self, k: NonZeroU64) -> Option<&u64> { self.get(&k) }
 
   #[inline(always)]
   fn insert(&mut self, k: NonZeroU64, v: u64) { let _: Option<_> = self.insert(k, v); }
